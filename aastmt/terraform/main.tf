97c78d6c4af097517a4dd2fcd44550f5f5c64f70
eslam411 <63361730+eslam411@users.noreply.github.com>
2023-12-31 22:08:51 +0200

    add terraform

---
+++ aastmt/terraform/main.tf
@@ -0,0 +1,53 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+
+
+
+# Create a resource group
+resource "azurerm_resource_group" "res1" {
+  name     = "EslamRes1"
+  location = "East Us"
+}
+
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "terraform-aks1"
+  location            = azurerm_resource_group.res1.location
+  resource_group_name = azurerm_resource_group.res1.name
+  dns_prefix          = "aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
